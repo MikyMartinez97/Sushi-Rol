5607eabf2db415c657f1034721eb369ff39daf56
Fernando Garcia <fergdelacruz@gmail.com>
2026-05-12 20:32:53 +0200

    Added auth controller skeleton

---
+++ controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export function registerUSer(req, res) {
+    res.send("User registered!");
+}
+
+export function login(req, res) {
+    res.send("Logged in");
+}
+
+export function logout(req, res) {
+    res.send("Logged out");
+}
+
+export function getProfile(req, res) {
+    res.send("User profile");
+}
