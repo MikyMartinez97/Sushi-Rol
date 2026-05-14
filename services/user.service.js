998f6d2df052e1ebecb0d00380fcbed14f6e00a4
f-GC <fergdelacruz@gmail.com>
2026-05-14 10:00:40 +0200

    Adds update user service

--- services/user.service.js
+++ services/user.service.js
@@ -18,3 +18,22 @@ export async function createUser({ name, email, password, role }) {
         data: { name, email, passwordHash, role: role ?? 'customer'}
     });
 }
+
+export async function updateUser(id, { name, email, password }) {
+    const data = {}
+
+    if (name) data.name = name;
+    if (email) data.email = email;
+    if (password) data.passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+
+    if (Object.keys(data).length === 0) {
+        const err = new Error('No fileds provided to update');
+        err.status = 400;
+        throw err; 
+    }
+
+    return db.user.update({
+        where: { id },
+        data,
+    });
+}
