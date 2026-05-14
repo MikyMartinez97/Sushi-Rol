b9c377eab52fba46c04a95f988da5bf24d7f8ebd
f-GC <fergdelacruz@gmail.com>
2026-05-14 09:36:53 +0200

    Adds create user service

--- services/user.service.js
+++ services/user.service.js
@@ -1,4 +1,7 @@
 import db from '../config/db.js';
+import bcrypt from 'bcrypt';
+
+const SALT_ROUNDS = 14;
 
 export async function getUsers() {
     return db.user.findMany({ orderBy: { name: 'asc' } })
@@ -6,4 +9,12 @@ export async function getUsers() {
 
 export async function getUserById(id) {
     return db.user.findUnique({ where: {id} });
+}
+
+export async function createUser({ name, email, password, role }) {
+    const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+
+    return db.user.create({
+        data: { name, email, passwordHash, role: role ?? 'customer'}
+    });
 }
