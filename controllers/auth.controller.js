0fa55b7f19a3f18f68e9f1a0e711ee9cbf1da9b0
f-GC <fergdelacruz@gmail.com>
2026-05-18 13:38:23 +0200

    Adds login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,36 @@
-export function registerUSer(req, res) {
+export async function registerUSer(req, res, next) {
     res.send("User registered!");
 }
 
-export function login(req, res) {
-    res.send("Logged in");
+export async function login(req, res, next) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Email and password are required' });
+        }
+
+        const { user, token } = await authService.login(email, password);
+
+        // Set JWT as httpOnly cookie
+        res.cookie('token', token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge:   60 * 60 * 1000, // 1 hour in milliseconds
+        });
+
+        res.status(200).json({
+            user: { id: user.id, name: user.name, email: user.email, role: user.role }
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
-export function logout(req, res) {
+export async function logout(req, res, next) {
     res.send("Logged out");
 }
 
-export function getProfile(req, res) {
+export async function getProfile(req, res, next) {
     res.send("User profile");
 }
