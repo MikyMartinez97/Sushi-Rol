42eb961d9c4ca087dfe5eba9b629c40f22674898
f-GC <fergdelacruz@gmail.com>
2026-05-19 13:50:35 +0200

    Adds list categories to category service

--- services/category.service.js
+++ services/category.service.js
@@ -1,8 +1,19 @@
 export async function getCategoryById(id) {
-    return db.category.findUnique({ 
+    return db.category.findUnique({
         where: { id },
         include: {
             _count: { select: { products: { where: { isActive: true } } } }
         },
+    });
+}
+
+export async function listCategories() {
+    return db.category.findMany({
+        orderBy: { name: 'asc' },
+        include: {
+            _count: {
+                select: { products: { where: { isActive: true } } }
+            }
+        }
     });
 }
