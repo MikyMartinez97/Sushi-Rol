6828f6e01a7090b23438f7e5eadb0bfd38b847ec
f-GC <fergdelacruz@gmail.com>
2026-05-19 12:32:40 +0200

    Adds getCategories to category controller

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -2,17 +2,22 @@ import * as categoryService from "../services/category.service.js"
 
 export async function getCategoryById(req, res, next) {
     try {
-       const category = await categoryService.getCategoryById(req.params.id);
+        const category = await categoryService.getCategoryById(req.params.id);
         if (!category) return res.status(404).json({ error: "Category not found" });
-        res.json(); 
+        res.status(200).json(category);
     } catch (err) {
         next(err);
     }
-    
+
 }
 
 export async function getCategories(req, res, next) {
-    res.send('Categories list');
+    try {
+        const categories = await categoryService.listCategories();
+        res.status(200).json({ categories });
+    } catch (err) {
+        next(err);
+    }
 }
 
 export async function createCategory(req, res, next) {
